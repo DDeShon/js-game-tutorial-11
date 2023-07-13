7f030c81946a63c4dc8a273978343208c496785a
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-07-13 13:47:03 -0500

    working on player movement

--- player.js
+++ player.js
@@ -1,18 +1,40 @@
-import { StandingLeft, StandingRight } from "./state.js";
+import {
+  StandingLeft,
+  StandingRight,
+  SittingLeft,
+  SittingRight,
+  RunningLeft,
+  RunningRight,
+  JumpingLeft,
+  JumpingRight,
+} from "./state.js";
 
 export default class Player {
   constructor(gameWidth, gameHeight) {
     this.gameWidth = gameWidth;
     this.gameHeight = gameHeight;
-    this.states = [new StandingLeft(this), new StandingRight(this)];
+    this.states = [
+      new StandingLeft(this),
+      new StandingRight(this),
+      new SittingLeft(this),
+      new SittingRight(this),
+      new RunningLeft(this),
+      new RunningRight(this),
+      new JumpingLeft(this),
+      new JumpingRight(this),
+    ];
     this.currentState = this.states[1];
     this.image = document.getElementById("dogImage");
     this.width = 200;
     this.height = 181.83;
     this.x = this.gameWidth / 2 - this.width / 2;
     this.y = this.gameHeight - this.height;
+    this.velocityY = 0;
+    this.weight = 0.5;
     this.frameX = 0;
     this.frameY = 0;
+    this.speed = 0;
+    this.maxSpeed = 10;
   }
   draw(context) {
     context.drawImage(
@@ -29,9 +51,24 @@ export default class Player {
   }
   update(input) {
     this.currentState.handleInput(input);
+    // horizontal movement
+    this.x += this.speed;
+    if (this.x <= 0) this.x = 0;
+    else if (this.x >= this.gameWidth - this.width)
+      this.x = this.gameWidth - this.width;
+    // vertical movement
+    this.y += this.velocityY;
+    if (!this.onGround()) {
+      this.velocityY += this.weight;
+    } else {
+      this.velocityY = 0;
+    }
   }
   setState(state) {
     this.currentState = this.states[state];
     this.currentState.enter();
+  }
+  onGround() {
+    return this.y >= this.gameHeight - this.height;
   }
 }
